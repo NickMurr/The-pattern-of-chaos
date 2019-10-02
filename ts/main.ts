c444d3cf98af8a2c2af4f9d901abdd4ba26bee84
nickmurr <32221055+NickMurr@users.noreply.github.com>
2019-10-02 20:42:37 +0300

    init

---
+++ ts/main.ts
@@ -0,0 +1,61 @@
+let current = 1;
+enum Num {
+  a = 1,
+  b,
+  c,
+}
+const init: any = {
+  a: [5, 5],
+  b: [460, 5],
+  c: [230, 300],
+};
+
+let d = [240, 160];
+
+let canvas: HTMLCanvasElement = document.getElementById("myCanvas");
+canvas.width = 480;
+canvas.height = 320;
+let ctx = canvas.getContext("2d")!;
+const { a, b, c } = init;
+ctx.beginPath();
+ctx.arc(a[0], a[1], 5, 0, Math.PI * 2, false);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(b[0], b[1], 5, 0, Math.PI * 2, false);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(c[0], c[1], 5, 0, Math.PI * 2, false);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 5, 0, Math.PI * 2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(360, 80, 5, 0, Math.PI * 2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+setInterval(() => {
+  const currentDot = Math.floor(Math.random() * 3 + 1);
+  const x = init[Num[currentDot]];
+  const newItem = [Math.abs(x[0] + d[0]) / 2, Math.abs(x[1] + d[1]) / 2];
+  d = newItem;
+
+  ctx.beginPath();
+  ctx.arc(newItem[0], newItem[1], 1, 0, Math.PI * 2, false);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.closePath();
+}, 1);
