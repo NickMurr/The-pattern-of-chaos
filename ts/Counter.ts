8d9456a40446a26062182337f35bcb9c13b7356e
nickmurr <32221055+NickMurr@users.noreply.github.com>
2019-10-03 11:42:36 +0300

    refactor

---
+++ ts/Counter.ts
@@ -0,0 +1,64 @@
+export interface ICanvas {
+  dimensions: { x: number; y: number };
+  canvasId: string;
+  init: { [key: string]: [number, number] };
+  start: [number, number];
+}
+
+export default class Canvas {
+  public canvas: HTMLCanvasElement;
+  public ctx: CanvasRenderingContext2D;
+  public init: { [key: string]: [number, number] };
+  public startPoint: [number, number];
+  public initCount: number;
+
+  constructor(options: ICanvas) {
+    this.canvas = document.getElementById(options.canvasId)!;
+    this.canvas.width = options.dimensions.x;
+    this.canvas.height = options.dimensions.y;
+    this.ctx = this.canvas.getContext("2d")!;
+    this.init = options.init;
+    this.initCount = Object.keys(this.init).length;
+    this.startPoint = options.start;
+    this.drawInit();
+    this.drawDots();
+  }
+
+  public drawInit() {
+    Object.keys(this.init).forEach((i) => {
+      this.ctx.beginPath();
+      this.ctx.arc(this.init[i][0], this.init[i][1], 5, 0, Math.PI * 2, false);
+      this.ctx.fillStyle = "red";
+      this.ctx.fill();
+      this.ctx.closePath();
+    });
+  }
+
+  public random() {
+    return Math.floor(Math.random() * this.initCount + 1);
+  }
+
+  public publicRandomCoordinates() {
+    return this.init[Object.keys(this.init)[this.random() - 1]];
+  }
+
+  public drawDots() {
+    // for (let i = 0; i < 20000; i++) {
+    const x = this.publicRandomCoordinates();
+    const d = this.startPoint;
+    const newItem = [Math.abs(x[0] + d[0]) / 2, Math.abs(x[1] + d[1]) / 2];
+    this.startPoint = newItem;
+    this.ctx.beginPath();
+    this.ctx.arc(newItem[0], newItem[1], 1, 0, Math.PI * 2, false);
+    this.ctx.fillStyle = "green";
+    this.ctx.fill();
+    this.ctx.closePath();
+    // }
+  }
+
+  public draw() {
+    for (let i = 0; i < 100000; i++) {
+      this.drawDots();
+    }
+  }
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,54 +1,27 @@
-let current = 1;
-let count = 0;
-enum Num {
-  a = 1,
-  b,
-  c,
-}
-const init: any = {
-  a: [5, 5],
-  b: [460, 5],
-  c: [230, 300],
-};
+import Canvas from "./Counter";
 
-let d = [240, 160];
+// const c = new Canvas({
+//   dimensions: { x: 1900, y: 900 },
+//   canvasId: "myCanvas",
+//   init: {
+//     a: [5, 5],
+//     b: [1875, 5],
+//     c: [950, 895],
+//   },
+//   start: [240, 160],
+// });
 
-let canvas: HTMLCanvasElement = document.getElementById("myCanvas");
-canvas.width = 480;
-canvas.height = 320;
-let ctx = canvas.getContext("2d")!;
-const { a, b, c } = init;
-ctx.beginPath();
-ctx.arc(a[0], a[1], 5, 0, Math.PI * 2, false);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(b[0], b[1], 5, 0, Math.PI * 2, false);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(c[0], c[1], 5, 0, Math.PI * 2, false);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.closePath();
-
-const counter = document.getElementById("count");
+const c = new Canvas({
+  dimensions: { x: 1900, y: 900 },
+  canvasId: "myCanvas",
+  init: {
+    a: [950, 5],
+    b: [1875, 895],
+    c: [5, 895],
+  },
+  start: [240, 160],
+});
 
 setInterval(() => {
-  count = count + 1;
-  const currentDot = Math.floor(Math.random() * 3 + 1);
-  const x = init[Num[currentDot]];
-  const newItem = [Math.abs(x[0] + d[0]) / 2, Math.abs(x[1] + d[1]) / 2];
-  d = newItem;
-
-  ctx.beginPath();
-  ctx.arc(newItem[0], newItem[1], 1, 0, Math.PI * 2, false);
-  ctx.fillStyle = "green";
-  ctx.fill();
-  ctx.closePath();
-  counter.textContent = count;
+  c.drawDots();
 }, 1);
